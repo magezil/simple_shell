20610436a35b66a83186906910bbdc86999bd852
Lee Gaines <eightlimbed@gmail.com>
2017-11-15 23:50:50 +0000

    adds non_interactive, env, and exit functions

--- shell.h
+++ shell.h
@@ -4,13 +4,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #define BUF_SIZE 200
 #define ARG_SIZE 10
+extern char **environ;
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *get_line(void);
 char **tokenize_line(char *line);
 void non_interactive_mode(void);
+int _strcmp(char *s1, char *s2);
+int _putchar(int c);
+void printenv(void);
 #endif
